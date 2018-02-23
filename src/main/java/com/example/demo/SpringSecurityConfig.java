e5ae90c4492c07ad97412d81536f978ac3668cde
lirh <lirh83@hotmail.com>
2018-02-23 21:53:52 +0800

    init

--- src/main/java/com/example/demo/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,13 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");//基于内存的验证
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll() //主目录
+        http.authorizeRequests()
+                .antMatchers("/").permitAll() //主目录
                 .anyRequest().authenticated()
                 .and()
-                .logout()//注销
+                .logout().permitAll()//注销
                 .and()
                 .formLogin(); //登陆
+        http.csrf().disable();//关闭csrf认证
     }
 
     @Override
